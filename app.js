3fe2d7c9717f362dec6ba21b812dbd0f71e17595
Tiago Laibacas <tiago.abreu.laibacas@gmail.com>
2024-06-01 08:11:04 +0100

    Done!

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+    response.sendFile (__dirname + '/public/index.html')
+})
+
+app.get('/About', (request, response) => {
+    response.sendFile ('about.html')
+})
+
+app.get('/Works', (request, response) => {
+    response.sendFile ('works.html')
+})
+
+app.listen(port) 
